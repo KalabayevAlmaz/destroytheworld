97f3833a8a85a0beed38219733a0c5533d32d2f8
Almaz Kalabayev <kalabayev.almaz@gmail.com>
2019-05-21 23:37:48 +0600

    Initial Commit

---
+++ destroy-the-world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  destroy-the-world
+//
+//  Created by Almaz on 5/21/19.
+//  Copyright © 2019 Almaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
